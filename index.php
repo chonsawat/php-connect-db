c1e3e765b2df10f5eff41f8f1bb0c9e123d8bb33
chonsawat <chonsawat.nakanam@kkumail.com>
2023-09-25 02:15:54 +0700

    Initial Setup

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Home Page</h1>
+  <?php
+  include 'service\db_connection.php';
+  $conn = OpenCon();
+  echo "Connected Successfully";
+  CloseCon($conn);
+  ?>
+</body>
+
+</html>
